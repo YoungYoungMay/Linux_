6451e1e344e9f4b6ee9a8fb4db0cb6087bd2c9ce
YoungMay_ <102332172@qq.com>
2018-07-12 14:45:44 +0800

    守护进程版本的HTTP服务器——传递消息

---
+++ class12/http/myhttpd.c
@@ -0,0 +1,100 @@
+//HTTP服务器，基于TCP协议
+//守护进程版本
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <strings.h>//bzero清空
+#include <string.h>
+#include <sys/wait.h>
+
+void service(int sock)
+{//对客户端的请求不做任何分析，均响应回去一个"hello,..."
+    char buf[10240];
+    read(sock, buf, sizeof(buf));
+    //HTTP响应：给一个响应行、一个空行,正文，不给响应报头
+    //HTTP的1.0版本是基于短链接的
+    const char* response = "HTTP/1.0 200 OK\n\n<html><head>hello, this is YoungMay</head></html>\n";
+    write(sock, response, strlen(response));
+}
+
+int startup(int port)
+{
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock < 0)
+    {
+        printf("socket error\n");
+        exit(2);
+    }
+
+    struct sockaddr_in local;
+    local.sin_family = AF_INET;
+    local.sin_port = htons(port);
+    //参数 宏INADDR_ANY，可以监听主机所有IP
+    local.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    //防止因为time_wait导致的bind失败
+    int opt = 1;
+    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    if(bind(sock, (struct sockaddr*)&local, sizeof(local)) < 0)
+    {
+        printf("bind error\n");
+        exit(3);
+    }
+
+    if(listen(sock, 5) < 0)
+    {
+        printf("listen error\n");
+        exit(4);
+    }
+    return sock;
+}
+
+//./myhttpd 8080
+int main(int argc, char* argv[])
+{
+    if(argc != 2)
+    {
+        printf("Usage: %s [port]\n", argv[0]);
+        return 1;
+    }
+    
+    int listen_sork = startup(atoi(argv[1]));
+    
+    daemon(0, 0);
+
+    while(1)
+    {
+        struct sockaddr_in peer;
+        bzero(&peer, sizeof(peer));
+        socklen_t len = sizeof(peer);
+        int sock = accept(listen_sork, (struct sockaddr*)&peer, &len);
+        if(sock < 0)
+        {
+            //write log
+            continue;
+        }
+        int id = fork();
+        if(id == 0)//child
+        {
+            close(listen_sork);
+            if(fork() > 0)
+            {
+                exit(0);
+            }
+            service(sock);
+            close(sock);
+            exit(0);
+        }
+        else if(id > 0)//father
+        {
+            close(sock);
+            waitpid(id, NULL, 0);
+        }
+    }
+    return 0;
+}
